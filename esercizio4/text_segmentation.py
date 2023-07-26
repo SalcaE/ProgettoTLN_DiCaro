d06ba54f307370b765be845a0968977b107bd75b
SpanuAlessandro <SpanuAlessandro@users.noreply.github.com>
2023-07-26 10:57:45 +0200

    alechan

--- esercizio4/text_segmentation.py
+++ esercizio4/text_segmentation.py
@@ -4,6 +4,9 @@ from rich.console import Console
 from rich.table import Table
 from nltk.tokenize import sent_tokenize
 
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+from sklearn import preprocessing
 
 
 def text_processing(filename):
@@ -26,12 +29,83 @@ def text_processing(filename):
                         len(token) >= 3] #vedere se tenere
                         for sent in sents]
 
-    return texts
+    return tokenized_sents
+
+def vocabulary(sentences):
+    tupla=()
+    meh=[]
+    mehmeh=[]
+    riempiScore=[]
+    for i in range(0,len(sentences)):
+        if i+1 < len(sentences):#scorro fino all'ultimo
+            
+            x=list(set(sentences[i])-set(sentences[i+1]))
+            x1=list(set(sentences[i+1])-set(sentences[i]))
+
+            ciao = set(sentences[i+1])
+            prova= [f for f in sentences[i] if f not in ciao ]
+
+            for word in sentences[i]:
+                if word not in sentences[i+1]:
+                    meh.append(word)
+
+            for word in sentences[i+1]:
+                if word not in sentences[i]:
+                    meh.append(word)
+
+            score = len(meh)/(len(sentences[i])+len(sentences[i+1]))
+                #if word in sentences[i+1]:
+                    #sentences[i].remove(word)
+                    #sentences[i+1].remove(word)
+                    #lollino = 1
+
+                    #z= sentences[i].index(word)
+                    #z1 = sentences[i+1].index(word)
+                    #sentences[i].pop(z)
+                    #sentences[i+1].pop(z)
+            #tupla= (i,i+1)
+            tupla= (i,i+1,score)
+            mehmeh.append(tupla)
+            riempiScore.append([score])
+           # meh.append(tupla)
+  
+
+
+    #fare uno score [0,1],[1,2]
+
+    #normalizzo
+    print("cazzo-NP")
+    #x_norm = (riempiScore-np.min(riempiScore))/(np.max(riempiScore)-np.min(riempiScore))
+ 
+    t=[]
+    for a in mehmeh:
+        x_norm = (a[2]-np.min(riempiScore))/(np.max(riempiScore)-np.min(riempiScore))
+        i= mehmeh.index(a)
+        tt=(a[0],a[1],x_norm)
+        mehmeh[i] = tt
+        t.append(x_norm)
+      
+        
+
+    print(mehmeh)
+
+
+    #prendo il minimo 3 volte(non credo vada...)
+    print("valori minimi")
+    print(sorted(t)[1])
+    print(sorted(t)[2])
+    print(sorted(t)[3])
+
+    pene = 20
+    
+
+
 
 #tupla {(lf,rf,score),...}
 
 def main():
     sentences = text_processing("esercizio4/sample.txt")
+    vocabulary(sentences)
     
 
 if __name__ == '__main__':
