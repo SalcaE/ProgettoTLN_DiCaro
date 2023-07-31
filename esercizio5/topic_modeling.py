728d72e48149fc7664b518fc530f608395240683
Max26@15Edo <eduard.salca@edu.unito.it>
2023-07-31 17:20:36 +0200

    es 5 finito

--- esercizio5/topic_modeling.py
+++ esercizio5/topic_modeling.py
@@ -14,30 +14,34 @@ def text_processing(file_location):
     stop = set(stopwords.words('english')+list(string.punctuation)+['--','-','``',"\""])
     for file_name in os.listdir(file_location):
         with open(file_location + file_name, 'r') as f:
-            print(file_name)
             text = f.read().replace("\n",' ')
             tmp = nltk.word_tokenize(text)
             tokens = [doc[0].lower() for doc in nltk.pos_tag(tmp) if doc[0] not in stop and doc[1] in ['NN','NNS','NNP','NNPS']]
             texts.append(tokens)
+    return texts
 
-    num_topics = 15
+
+def lda_process(texts):
     dictionary = corpora.Dictionary(texts)
-    corpussss = [dictionary.doc2bow(text) for text in texts]
-    lda_model_15 = gensim.models.LdaMulticore(corpus=corpussss, id2word=dictionary, num_topics=15)
-    lda_model_10 = gensim.models.LdaMulticore(corpus=corpussss, id2word=dictionary, num_topics=10)
-    lda_model_20 = gensim.models.LdaMulticore(corpus=corpussss, id2word=dictionary, num_topics=20)
+    corpus = [dictionary.doc2bow(text) for text in texts]
+
+    lda_model_15 = gensim.models.LdaMulticore(corpus=corpus, id2word=dictionary, num_topics=15)
+    lda_model_20 = gensim.models.LdaMulticore(corpus=corpus, id2word=dictionary, num_topics=20)
+
+    lda_15 = gensimvis.prepare(lda_model_15, corpus, dictionary)
+    lda_20 = gensimvis.prepare(lda_model_20, corpus, dictionary)
+
+    pyLDAvis.save_html(lda_15,'esercizio5/htmlprova15.html')
+    pyLDAvis.save_html(lda_20,'esercizio5/htmlprova20.html')
+
     pprint(lda_model_15.print_topics())
-    pprint(lda_model_10.print_topics())
+    print('')
     pprint(lda_model_20.print_topics())
-    #lda = gensimvis.prepare(lda_model, corpussss, dictionary)
-
-
 
 def main():
-
-    text_processing("esercizio5/data/")
+    texts = text_processing("esercizio5/data/")
+    lda_process(texts)
 
     
-
 if __name__ == '__main__':
    main()
