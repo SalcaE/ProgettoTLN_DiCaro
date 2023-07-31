ae031e717c0c497c3fe6635d1f780750e4125c61
Max26@15Edo <eduard.salca@edu.unito.it>
2023-07-31 12:00:34 +0200

    es 5

--- esercizio5/topic_modeling.py
+++ esercizio5/topic_modeling.py
@@ -1,19 +1,34 @@
 import gensim
+from gensim import corpora
 import nltk
 nltk.download('stopwords')
-from nltk.corpus import gutenberg
-print(gutenberg.fileids())
-nltk.download()
+import os
+from nltk.corpus import stopwords
+import string
+import pyLDAvis
+import pyLDAvis.gensim_models as gensimvis
 
 def text_processing(file_location):
-    text=[]
-    stop = set(stopwords.words('english'))
-    return True
+    texts=[]
+    stop = set(stopwords.words('english')+list(string.punctuation)+['--','-','``',"\""])
+    for file_name in os.listdir(file_location):
+        with open(file_location + file_name, 'r') as f:
+            text = f.read().replace("\n",' ')
+            sapo = [doc.lower() for doc in nltk.word_tokenize(text) if doc not in stop]
+            texts.append(sapo)
+
+    num_topics = 15
+    dictionary = corpora.Dictionary(texts)
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    lda_model = gensim.models.LdaMulticore(corpus=corpus, id2word=dictionary, num_topics=num_topics)
+    pyLDAvis.enable_notebook()
+    gensimvis.prepare(lda_model, corpus, dictionary, mds='mmds')
+    return texts
 
 
 def main():
 
-    tokenized_sents = text_processing("esercizio4/texts.txt")
+    tokenized_sents = text_processing("esercizio5/data/")
 
     
 
