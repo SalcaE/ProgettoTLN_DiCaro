37aa2a00b67546da45c35cae715979734ecc5a57
Max26@15Edo <eduard.salca@edu.unito.it>
2023-07-31 10:31:55 +0200

    sapo

--- esercizio5/topic_modeling.py
+++ esercizio5/topic_modeling.py
@@ -3,3 +3,17 @@ import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 
+def text_processing(file_location):
+    text=[]
+    stop = set(stopwords.words('english'))
+    return True
+
+
+def main():
+
+    tokenized_sents = text_processing("esercizio4/texts.txt")
+
+    
+
+if __name__ == '__main__':
+   main()
